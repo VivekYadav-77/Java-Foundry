e3b0890d40b2c2f79079e80cce2bd61a30352558
Vivek Yadav <shivamyadav9462@gmail.com>
2026-03-23 19:57:37 +0530

    PreviousGreatestElement problem

---
+++ Stack/PreviousGreatestElement.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class PreviousGreatestElement {
+    static void CalculatePGE(int a[]) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < a.length; i++) {
+            while ((!stack.isEmpty()) && (a[i] > a[stack.peek()])) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                System.out.println("- no PGE for" + a[i]);
+                stack.push(i);
+            } else {
+                System.out.println("PGE for" + a[i] + " is : " + a[stack.peek()]);
+                stack.push(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 12, 10, 20, 22, 15, 14, 18, 32, 20, 22, 19 };
+        CalculatePGE(a);
+    }
+}
