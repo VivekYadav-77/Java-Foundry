2d27fd48e969bde14f6cc43ae7e0146c02e2040c
Vivek Yadav <shivamyadav9462@gmail.com>
2026-09-11 15:15:05 +0530

    Tree creation code along with the traversal prorder traversel

--- Tree/TreeImplementationV1.java
+++ Tree/TreeImplementationV1.java
@@ -11,10 +11,10 @@ class Tree{
     }
 }
 class TreeCreation{
-    Tree create(Scanner sc){
+    Tree create(Scanner sc , String direction , int PreviousElement){
         while(true){
-            System.out.println("\nSelect which operation you want to perform");
-            System.out.println("Enter 1 to insert the element in tree : ");
+            System.out.println("\nSelect which operation you want to perform in the "+direction+" branch of the elemnt : "+PreviousElement);
+            System.out.println("Enter 1 to insert the element in the "+direction+"branch");
             System.out.println("Enter 0 for the exit");
             int input = sc.nextInt();
             switch (input) {
@@ -22,10 +22,8 @@ class TreeCreation{
                     System.out.println("Enter the data to be inserted in tree : ");
                     int data = sc.nextInt();
                     Tree newnode = new Tree( data) ;
-                    System.out.println("Enter the data in the LEFT branch of the "+data);
-                    newnode.left = create(sc);
-                    System.out.println("Enter the data in the RIGHT branch of the "+data);
-                    newnode.right = create(sc);
+                    newnode.left = create(sc,"Left",data);
+                    newnode.right = create(sc,"Right",data );
                     return newnode;
                 case 0:
                     return null ;
@@ -44,13 +42,28 @@ class TreeCreation{
 
 
     }
+    void display(Tree value){
+        if(value==null){
+            return ;
+        }
+        System.out.println("Element  "+value.data);
+        display(value.left);
+        display(value.right);
+
+    }
     
 }
 public class TreeImplementationV1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TreeCreation creation = new TreeCreation();
-        Tree root = creation.create(sc);
+        System.out.println("Enter the element in the root node : ");
+        int rootelemnt = sc.nextInt();
+        Tree newnode = new Tree(rootelemnt);
+        newnode.left = creation.create(sc, "LEFT", rootelemnt);
+        newnode.right = creation.create(sc, "RIGHT", rootelemnt);
+        
+        creation.display(newnode);
         sc.close() ;
 
         
