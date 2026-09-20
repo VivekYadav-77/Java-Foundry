39b95872c29a4fb17117f5bebd1296ece5d2cc7e
Vivek Yadav <shivamyadav9462@gmail.com>
2026-09-20 15:57:04 +0530

    First element which has the frequency 1

--- Practice/FrequencyOfElements.java
+++ Practice/FrequencyOfElements.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class FrequencyOfElements{
         public static void main(String[] args) {
         Map<Integer,Integer>freq  = new HashMap<>();
-        int arr[]= {2, 3, 2, 5, 3, 2};
+        int arr[]= {4, 2, 7, 2, 4, 9, 2};
         for(int num : arr){
             if(freq.containsKey(num)){
                 freq.put(num,freq.getOrDefault(num,0)+1);
@@ -13,6 +13,13 @@ public class FrequencyOfElements{
 
         }
         freq.forEach((key,value)->System.out.println("Element : "+key+" frequency is : "+value));
+
+
+        for(int num : arr){
+            if(freq.get(num)==1){
+                System.out.println("Element : "+num+" has the frequency 1 first ");
+            }
+        }
 
     }
 
