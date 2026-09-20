3ccd637adc61c826085be27871b48774e30910a0
Vivek Yadav <shivamyadav9462@gmail.com>
2026-09-20 16:20:08 +0530

    Detect the duplication in the array

--- Practice/FrequencyOfElements.java
+++ Practice/FrequencyOfElements.java
@@ -1,8 +1,10 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
 public class FrequencyOfElements{
         public static void main(String[] args) {
         Map<Integer,Integer>freq  = new HashMap<>();
+        HashSet<Integer> duplicate = new HashSet<>();
         int arr[]= {4, 2, 7, 2, 4, 9, 2};
         for(int num : arr){
             freq.put(num,freq.getOrDefault(num,0)+1);
@@ -15,6 +17,15 @@ public class FrequencyOfElements{
         for(int num : arr){
             if(freq.get(num)==1){
                 System.out.println("Element : "+num+" has the frequency 1 first ");
+                break;
+            }
+        }
+
+        //array contains any duplicate element.
+        for(int num : arr){
+            if(!duplicate.add(num)){
+                System.out.println("Duplicate exist in the arr ");
+                break;
             }
         }
 
